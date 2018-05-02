d26a901dac259323a7a29dc203b29a66e16ef28f
ablech <arek_b1993@poczta.fm>
2018-05-02 18:19:18 +0200

    initial commit & project setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let path = require('path');
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const SRC_DIR = path.resolve(__dirname, "src" );
+
+
+module.exports = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_DIR,
+                use: {
+                    loader: "babel-loader"
+                }
+
+            }
+        ]
+    }
+};
