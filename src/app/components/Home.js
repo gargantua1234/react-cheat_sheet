6f6cc682f86084471dbd686eb7d66f5f67b0b9ee
ablech <arek_b1993@poczta.fm>
2018-05-03 17:37:42 +0200

    working with data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,36 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 export class Home extends React.Component{
     render(){
+        let text = "something";
         return (
           <div>
               <p>In a new Component</p>
+              <p>{text}</p>
+              <p>your name is {this.props.name} your age is {this.props.age}</p>
+              <p>User Object => Name: {this.props.user.name}</p>
+              <div>
+                  <h4>Hobbies</h4>
+                  <ul>
+                      {this.props.user.hobbies.map((hobby, i) =>
+                        <li key={i}>{hobby}</li>
+                      )}
+                  </ul>
+              </div>
+              <hr/>
+              {/*odwolanie sie elementu zagniezdzonego*/}
+              {this.props.children}
           </div>
         );
     }
 }
+
+
+//sprawdzanie czy w pliku index.js typ zmiennych podanych kompnentowi
+//Home jest proprawny
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user:PropTypes.object,
+    children: PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,10 @@ import { Home} from "./components/Home";
 
 class App extends React.Component{
     render(){
+        let user = {
+            name: "Anna",
+            hobbies: ["Sports", "Reading"]
+        };
         return(
             <div className="container">
                 <div className="row">
@@ -16,7 +20,9 @@ class App extends React.Component{
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={27} user={user}>
+                            <p>This is a paragraphs</p>
+                        </Home>
                     </div>
                 </div>
             </div>
